4f40d962cdd9f1acd1d84632cd8d905b21cbd6ff
Rafael <rafasousa@gmail.com>
2021-06-10 11:55:07 -0300

    melhora cadastro de biometria

--- src/main/java/br/com/zup/orangetalents/proposta/cartao/validation/Base64Validator.java
+++ src/main/java/br/com/zup/orangetalents/proposta/cartao/validation/Base64Validator.java
@@ -4,13 +4,19 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import static java.util.Base64.getDecoder;
+import static java.util.Base64.getEncoder;
 
 public class Base64Validator implements ConstraintValidator<Base64, String> {
 
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
 		try {
-			getDecoder().decode(value);
+			byte[] decoded = getDecoder().decode(value);
+			String encoded = getEncoder().encodeToString(decoded);
+			
+			if (!value.equals(encoded)) {
+				return false;
+			}
 		} catch (IllegalArgumentException ex) {
 			return false;
 		}
