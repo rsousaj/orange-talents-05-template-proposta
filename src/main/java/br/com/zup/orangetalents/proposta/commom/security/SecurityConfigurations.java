6f9d74626cc0ba1f1a78340ab149b300b79a37a0
Rafael <rafasousa@gmail.com>
2021-06-11 14:06:05 -0300

    implanta a API como um resource server utilizando KeyCloak

---
+++ src/main/java/br/com/zup/orangetalents/proposta/commom/security/SecurityConfigurations.java
@@ -0,0 +1,30 @@
+package br.com.zup.orangetalents.proposta.commom.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+	
+	@Value("${proposta.proposta.uri}")
+	private String uriPropostas;
+	
+	@Value("${proposta.cartao.uri}")
+	private String uriCartoes;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests((authorizeRequests) -> 
+			authorizeRequests
+				.antMatchers(HttpMethod.GET, uriPropostas).hasAuthority("SCOPE_propostas:read")
+				.antMatchers(HttpMethod.POST, uriPropostas).hasAuthority("SCOPE:propostas:write")
+				.antMatchers(HttpMethod.GET, uriCartoes).hasAuthority("SCOPE:cartoes:read")
+				.antMatchers(HttpMethod.POST, uriCartoes).hasAuthority("SCOPE:cartoes:write")
+				.anyRequest().authenticated()
+			)
+			.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+	}
+
+}
